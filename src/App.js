247e31c262eea8d3834583796d35fa6d00af0991
Louise Huang <louise_hy_huang@wistron.com>
2022-09-04 08:02:46 +0000

    feat(ProtectedRoute): add protected route to protect the todo list page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { AuthContext } from './Context'
 import './assets/all.css'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
+import ProtectedRoute from './components/ProtectedRoute'
 import TodoList from './components/TodoList'
 import NotFound from './components/NotFound'
 
@@ -16,7 +17,9 @@ function App() {
         <Routes>
           <Route index element={<Login />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
+          <Route element={<ProtectedRoute/>}>
             <Route path="/todo" element={<TodoList />}></Route>
+          </Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </AuthContext.Provider>
